3a1f45370e8e83b3d302e8e5be1c2e1d9e285414
Supreet Singh <supreet04singh@gmail.com>
2024-01-20 02:28:52 +0530

    Setting up backend part for project

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+const server = require("http").createServer(app);
+const { Server } = require("socket.io");
+
+const io = new Server(server);
+const port = process.env.PORT || 5000;
+
+
+// routes
+app.get("/", (req, res) =>{
+    res.send("This is mern white board sharing app server by our team");
+});
+io.on("connection", (socket) => {
+    console.log("user connected");
+});
+
+server.listen(port, () => console.log("server is running on http://localhost:5000"));
