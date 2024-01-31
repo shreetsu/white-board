1fc893b343b87a93445b87f59b928caf17094238
Supreet Singh <supreet04singh@gmail.com>
2024-01-31 22:45:39 +0530

    backend updated and connections from home page to room

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,37 @@
 import Forms from './components/Forms/index.jsx';
-import{ Route, Routes, UNSAFE_useRouteId } from "react-router-dom";
+import { Route, Routes, UNSAFE_useRouteId } from "react-router-dom";
+import io from "socket.io-client";
 import "./App.css";
 import RoomPage from "./pages/RoomPage"
+import { useEffect, useState } from 'react';
+
+const server = "http://localhost:5001";
+const connectionOptions = {
+  "force new connection " : true,
+  reconnectionAttempts : "Infinity",
+  timeout : 10000,
+  transports : ["websocket"],
+
+};
+
+const socket = io(server, connectionOptions);
 
 const App = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    socket.on("userIsJoined", (data) => {
+      
+      if(data.success){
+        console.log("userJoined");
+      }
+      else{
+        console.log("userJoined error");
+      }
+      setUser(data);
+
+    })
+  }, [])
 
   const uuid = () => {
     let s4 = () => {
@@ -28,7 +56,7 @@ const App = () => {
   return (
     <div className="container">
       <Routes>
-        <Route path="/" element={<Forms uuid = {uuid}/>} />
+        <Route path="/" element={<Forms uuid = {uuid} socket = {socket} setUser={setUser}/>} />
         <Route path="/:roomId" element={<RoomPage/>} />
       </Routes>
       
--- frontend/src/components/Forms/CreateRoomForm/index.jsx
+++ frontend/src/components/Forms/CreateRoomForm/index.jsx
@@ -1,14 +1,40 @@
 import { useState } from "react";
-
-const CreateRoomForm=({uuid})=>{
+import { useNavigate } from "react-router-dom";
+const CreateRoomForm=({uuid, socket, setUser})=>{
     const [roomId, setRoomId] = useState(uuid());
     const [name, setName] = useState("");
+
+    const navigate = useNavigate();
+
+
+    const handleCreateRoom = (e) => {
+        e.preventDefault();
+
+        // {name, roomId, userId, host, presenter}
+
+        const roomData = {
+            name, 
+            roomId,
+            userId : uuid(),
+            host : true,
+            presenter : true
+        };
+        setUser(roomData);
+        navigate(`/${roomId}`);
+        console.log(roomData);
+        socket.emit("userJoined", roomData);
+    };
+
     return (
         <form className="form col-md-12 mt-5">
             <div className="form-control">
                 <input type="text" 
                 className="form-control my-2"
-                placeholder="Enter your Name" />
+                placeholder="Enter your Name" 
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                />
+                
             </div>
             <div className="form-group border">
                 <div className="input-group d-flex align-items-center justify-content-center">
@@ -29,7 +55,9 @@ const CreateRoomForm=({uuid})=>{
                     <button className="btn btn-outline-danger btn-sm me-2" type="button">Copy</button>
                 </div>
             </div>
-            <button type="submit" className="mt-4 btn btn-primary btn-block form-control"> Generate Room </button>
+            <button type="submit"
+                onClick={handleCreateRoom}
+                className="mt-4 btn btn-primary btn-block form-control"> Generate Room </button>
         </form>
     )
 };
